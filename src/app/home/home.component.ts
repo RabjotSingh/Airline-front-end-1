677a3acafae8e543139c2d28339e9c974d811ef2
RabjotSingh <67277148+RabjotSingh@users.noreply.github.com>
2021-08-13 13:09:52 +0530

    frontend backend done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,10 @@
 import { FooterComponent } from './../footer/footer.component';
 import { HeaderComponent } from './../header/header.component';
 import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup,Validators} from '@angular/forms';
+import { FlightService } from '../flight.service';
+import { Flight } from '../models/Flight';
+
 
 @Component({
   selector: 'app-home',
@@ -9,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  flights:Flight[]=[];
+
+  flightsearchform =new FormGroup({
+    from:new FormControl("",[Validators.required]),
+    to:new FormControl("",[Validators.required]),
+    depart:new FormControl("",[Validators.required]),
+    arrive:new FormControl("",[Validators.required,]),
+  });
+
+  constructor(private obj:FlightService) { }
 
   ngOnInit(): void {
+  }
+
+  get_api():void
+  {
+    this.obj.getAllUsers().subscribe(data=>{
+      this.flights=data;
+      //Logging the response recieved from web api.
+      console.log(this.flights);
+    });
+  }
+
+  onSubmit()
+  {
+    this.get_api();
   }
 
 }
